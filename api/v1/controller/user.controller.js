8f5b7cb442b25b606fe4d8a12659fc9352d197a7
anhphan10 <phananhd09@gmail.com>
2024-11-22 16:06:45 +0700

    userInfo

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -155,3 +155,18 @@ module.exports.resetPassword = async (req,res) =>{
         message:"Đổi Mật Khẩu Thành Công"
     })
 }
+module.exports.detail = async (req,res) => {
+    const token = req.cookies.token;
+    try {
+        const user = await User.findOne({
+            token:token,
+            deleted: false
+        }).select("-password -token");
+        res.json({
+            code:200,
+            user: user
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
