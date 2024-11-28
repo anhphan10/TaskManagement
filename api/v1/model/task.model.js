d2a3dedf7c49c062b62eb9f23dfa4298021f45ae
anhphan10 <phananhd09@gmail.com>
2024-11-28 20:08:57 +0700

    Create child tasks and save the parent task id

--- api/v1/model/task.model.js
+++ api/v1/model/task.model.js
@@ -9,6 +9,7 @@ const taskSchema = new mongoose.Schema(
     timeFinish: Date,
     createdBy: String,
     listUser:[],
+    taskParentId : String,
     deleted:{
         type: Boolean,
         default: false
